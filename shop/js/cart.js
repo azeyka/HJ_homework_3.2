a84b806fe1cb8ab2ba2fb997a4023a3b493df948
Elena Azeeva <www.azeyka@mail.ru>
2019-02-21 14:29:46 +0800

    solved

--- shop/js/cart.js
+++ shop/js/cart.js
@@ -1,1 +1,19 @@
 'use strict';
+document.addEventListener('DOMContentLoaded', init);
+showMore.addEventListener('click', init);
+
+function init() {
+  const itemsList = document.querySelector('.items-list');
+  itemsList.addEventListener('click', onAddToCartClick);
+};
+
+function onAddToCartClick(event) {
+  if (event.target.tagName === 'A') {
+    event.stopPropagation();
+    const item = {
+      title: event.target.dataset.title,
+      price: event.target.dataset.price
+    };
+    addToCart(item);
+  };
+};
--- table/js/sort.js
+++ table/js/sort.js
@@ -1,5 +1,10 @@
 'use strict';
+const tableHead = document.querySelector('thead');
+tableHead.addEventListener('click', handleTableClick);
 
 function handleTableClick(event) {
-  
-}
+  event.stopPropagation(); 
+  event.target.dataset.dataDir === '1' ? event.target.dataset.dataDir = '-1' : 
+                                                 event.target.dataset.dataDir = '1';
+  sortTable(event.target.dataset.propName, +event.target.dataset.dataDir);
+};
